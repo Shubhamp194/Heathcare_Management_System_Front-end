be14f7a875c63df1f93c6e9dcfaf2e9d290298ea
Shubhamp194 <shubhampandey194@gmail.com>
2023-02-16 18:43:44 +0530

    added a h1 tag for testing new git branch

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1> Hi this is done by Shubham</h1>
     <App />
   </React.StrictMode>
 );
